c7cef1e2cb7cd52893349be9205159e5625a6a58
Jhonas028 <soterojhonas028@gmail.com>
2026-04-13 15:49:09 +0800

    such a fast paced coding

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { routesDeveloper } from "./routes/routesDeveloper";
+import { StoreProvider } from "./store/StoreContext";
 
 function App() {
   const queryClient = new QueryClient();
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        <Router>
-          <Routes>
-            <Route path="*" element=<>page not found.</> />
-          </Routes>
-        </Router>
+        <StoreProvider>
+          <Router>
+            <Routes>
+              <Route path="*" element=<>page not found.</> />
+              {routesDeveloper.map(({ ...routesProps }, key) => {
+                return <Route key={key} {...routesProps} />;
+              })}
+            </Routes>
+          </Router>
+        </StoreProvider>
       </QueryClientProvider>
     </>
   );
---
+++ src/pages/developer/Layout.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "../../partials/Header";
+import Navigation from "../../partials/Navigation";
+import { navList } from "./nav-function";
+
+const Layout = ({ children, menu = "", submenu = "" }) => {
+  return (
+    <>
+      {/* HEADER */}
+      <Header />
+      {/* NAVIGATION */}
+      <Navigation menu={menu} submenu={submenu} navigationList={navList} />
+      {/* BODY */}
+      {children}
+      {/* FOOTER */}
+    </>
+  );
+};
+
+export default Layout;
